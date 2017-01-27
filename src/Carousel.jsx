73f177e0811d606b81f9da13c2cbfd4ddbb56c80
Ryan Quinn <rquinn@circleup.com>
2017-01-26 19:38:48 -0800

    added pics

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -2,28 +2,43 @@ import React, {Component} from 'react';
 import Flickity from 'flickity/dist/flickity.pkgd';
 import 'flickity/dist/flickity.css';
 import jCole from '../Images/JCole-Final-1-2.jpg';
+import bridge from '../Images/bridge.jpg';
+import otherBridge from '../Images/bridge_2.jpg';
+import skyline from '../Images/skyline.jpg';
+import boat from '../Images/boat.jpg';
+import beach from '../Images/beach.jpg';
+import highway from '../Images/highway.jpg';
+
+const images = [
+    bridge,
+    highway,
+    otherBridge,
+    skyline,
+    boat,
+    jCole,
+    beach
+];
+
 class Carousel extends Component {
 
     componentDidMount() {
         const {carousel} = this.refs;
         new Flickity(carousel, {
-            // cellAlign: 'left',
-            //contain: true
+            prevNextButtons: false,
+            autoPlay: 5000,
+            pauseAutoPlayOnHover: false
         });
-
     }
 
     render() {
         return (
             <div className="carousel-container">
                 <div className="main-carousel" ref="carousel">
-                    <div className="carousel-cell carousel-image">
-                        <div className="image-container">
-                            <div className="image-item" style={{backgroundImage: `url(${jCole})`}}/>
+                    {images.map((i) => (
+                        <div key={i} className="carousel-cell carousel-image">
+                            <img src={i} width="100%"/>
                         </div>
-                    </div>
-                    <div className="carousel-cell carousel-image">...</div>
-                    <div className="carousel-cell carousel-image">...</div>
+                    ))}
                 </div>
             </div>
         );
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import classNames from 'classnames';
 import instagram from '../icons/instagram.png';
 import linkedIn from '../icons/linkedin.png';
+import gmail from '../icons/gmail.png';
+import sepp from '../icons/sepp-white.png';
 
 class Navigation extends Component {
 
@@ -12,35 +14,43 @@ class Navigation extends Component {
         };
     }
 
-    toggleExpandedContact(){
-        this.setState({ expandedContact: !this.state.expandedContact });
+    toggleExpandedContact() {
+        this.setState({expandedContact: !this.state.expandedContact});
     }
 
     render() {
-        const brandClasses = classNames('main-navigation__brand', {
-           'main-navigation__brand--clicked': this.state.expandedContact
+        const brandClasses = classNames('main-navigation__brand');
+
+        const contactClasses = classNames('main-navigation__item',{
+            'main-navigation__item--clicked': this.state.expandedContact
         });
         return (
             <nav className="main-navigation">
                 <div className={ brandClasses }>
-                    <h1 onClick={ () => this.toggleExpandedContact() }
-                        className="main-navigation__brand-title">
-                        Sepp Dasbach
+                    <h1 className="main-navigation__brand-title">
+                        <img className="sepp-logo" src={sepp}/>
                     </h1>
-                    <div className="contact-icon instagram" title="Instagram">
-                        <a href="https://www.instagram.com/seppsippycup/" target="_blank">
-                            <img src={instagram} width="20px"/>
-                        </a>
-                    </div>
-                    <div className="contact-icon linkedin" title="Linkedin">
-                        <a href="https://www.linkedin.com/in/sepp-dasbach-27274288" target="_blank">
-                            <img src={linkedIn} width="25px"/>
-                        </a>
-                    </div>
                 </div>
                 <div className="main-navigation__items">
-                    <div className="main-navigation__item">Work</div>
-                    <div className="main-navigation__item">Contact</div>
+                    <div onClick={ () => this.toggleExpandedContact() }
+                        className={ contactClasses }>
+                        Contact
+                        <div className="contact-icon instagram" title="Instagram">
+                            <a href="https://www.instagram.com/seppsippycup/" target="_blank">
+                                <img src={instagram} width="100%"/>
+                            </a>
+                        </div>
+                        <div className="contact-icon linkedin" title="Linkedin">
+                            <a href="https://www.linkedin.com/in/sepp-dasbach-27274288" target="_blank">
+                                <img src={linkedIn} width="100%"/>
+                            </a>
+                        </div>
+                        <div className="contact-icon email" title="Email">
+                            <a href="mailto:sippycup14@gmail.com" target="_blank">
+                                <img src={gmail} width="100%"/>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </nav>
         );
